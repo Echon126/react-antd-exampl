8013022e4028cf8b38b1b43ac6954d04b25f10cd
zhangwenjie <zhangwenjie@xaeport.com>
2017-03-07 16:52:29 +0800

    react and antd

---
+++ src/components/static.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// 结尾组件
+export default class myStatic extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="ani-box">
+                <div ></div>
+            </div>
+        )
+    }
+}
